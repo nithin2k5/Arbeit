e3962eafa7205eb44dff5189fb4dd397d64e291e
yaswanth5019k <yaswanthkalluri2005@gmail.com>
2025-09-22 15:12:39 +0530

    Update next.config.js

--- my-app/next.config.js
+++ my-app/next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    NEXT_PUBLIC_BACKEND_URL: 'http://localhost:8080/api',
+  },
   webpack: (config, { isServer }) => {
     // Add a rule to handle PDF.js worker
     config.resolve.alias = {
